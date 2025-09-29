70a9cd127cadad1352f7a2860736dafced25e667
MigueM531 <MigueM531>
2025-09-28 20:15:00 -0500

    creada clase coordinador

---
+++ src/model/Coordinador.py
@@ -0,0 +1,17 @@
+class Coordinador:
+    def __init__(self, nombre: str):
+        self.id: int
+        self.nombre = nombre
+
+    def aprobarCurso(self):
+        pass
+
+    def modificarCurso(self):
+        pass
+
+    def generarReporte(self):
+        pass
+
+    def crearCurso(self):
+        pass
+    
