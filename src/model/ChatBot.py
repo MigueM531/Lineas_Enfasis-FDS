083e0ec07e65fc5a3d5f534bced5dc03e1c0eb21
MigueM531 <MigueM531>
2025-09-28 20:15:07 -0500

    creada clase chatbot

---
+++ src/model/ChatBot.py
@@ -0,0 +1,16 @@
+class Chatbot:
+    def __init__(self, nombre: str):
+        self.idBot: int
+        self.nombre = nombre
+
+    def mostrarCursos():
+        pass
+
+    def validarInscripcion():
+        pass
+
+    def enviarNotificacion():
+        pass
+
+    def generarReporte():
+        pass
