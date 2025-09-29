19e86ef60afe4b07491b297e2a309af1963db9f0
MigueM531 <MigueM531>
2025-09-28 20:14:30 -0500

    creada clase estudiante

---
+++ src/model/Estudiante.py
@@ -0,0 +1,14 @@
+class Estudiante:
+    def __init__(self, nombre: str, programa: str):
+        self.id: int
+        self.nombre = nombre
+        self.programa = programa
+
+    def consultarCursos(self):
+        pass
+
+    def inscribirse(self):
+        pass
+
+    def cancelarCurso(self):
+        pass
